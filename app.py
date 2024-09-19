9d842d5bdda3384d837b0ccb457a46522e7c5713
ajayhanssen <andi.thoeni@gmail.com>
2024-09-19 16:23:51 +0200

    gotta hate tkinter

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from tkinter import ttk
 from PIL import Image, ImageTk
 import cv2
 
+from geometrical import *
+from field_recognizer import *
+from solver import *
+
 LARGEFONT = ('Verdana', 20)
 
 class SudokoSolverApp(tk.Tk):
@@ -49,11 +53,72 @@ class CamPage(ttk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
+        self.controller = controller
+        self.cap = cv2.VideoCapture(0)  # initialize the camera
+
         label = ttk.Label(self, text='Camera Page', font=LARGEFONT)
         label.pack(pady=10, padx=10)
 
-        back_button = ttk.Button(self, text='Back', command=lambda: controller.show_frame(MainPage))
+
+        # left frame for camera view
+        left_frame = ttk.Frame(self)
+        left_frame.pack(pady=10, padx=10, side=tk.LEFT)
+
+        self.video_label = ttk.Label(left_frame)
+        self.video_label.pack()
+
+        self.placeholder1 = ttk.Label(left_frame)
+        self.placeholder1.pack(pady=10, padx=10, expand=tk.TRUE)
+
+
+        # right frame for scan button
+        right_frame = ttk.Frame(self)
+        right_frame.pack(pady=10, padx=10, side=tk.RIGHT)
+
+        scan_button = ttk.Button(right_frame, text='Scan', command=self.scan)
+        scan_button.pack(pady=10, padx=10)
+
+        self.placeholder2 = ttk.Label(right_frame)
+        self.placeholder2.pack(pady=10, padx=10, expand=tk.TRUE)
+
+
+        bottom_frame = ttk.Frame(self)
+        bottom_frame.pack(pady=10, padx=10, side=tk.BOTTOM, expand=tk.TRUE)
+
+        back_button = ttk.Button(bottom_frame, text='Back', command=lambda: controller.show_frame(MainPage))
         back_button.pack(fill=tk.BOTH, expand=tk.TRUE)
+
+        self.update_frame()
+    
+    def update_frame(self):
+        ret, frame = self.cap.read()
+        if ret:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            self.original_img = cv2.imread('puzzles/puzzle_1_persp.jpeg') #-----------------------------testing only
+
+            frame = cv2.resize(frame, (320, 240))
+            img = Image.fromarray(frame)
+            imgtk = ImageTk.PhotoImage(image=img)
+            self.video_label.imgtk = imgtk
+            self.video_label.configure(image=imgtk)
+        self.after(25, self.update_frame)  # Update every 10 ms
+    
+    def scan(self):
+        
+        img = Image.fromarray(un_warp_sudoku(self.original_img))
+        self.unwarped_img = img
+        img = img.resize((320, 320))
+        imgtk = ImageTk.PhotoImage(image=img)
+
+        self.placeholder1.imgtk = imgtk
+        self.placeholder1.configure(image=imgtk)
+
+        self.placeholder2.imgtk = imgtk
+        self.placeholder2.configure(image=imgtk)
+
+    def __del__(self):
+        if self.cap.isOpened():
+            self.cap.release()
 
 class LoadPage(ttk.Frame):
 
